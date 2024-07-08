259bb395b10eb3f19dab1e960f19a576298be2c9
PavelRubtcov <pavel.rubtcov11@mail.ru>
2024-07-08 18:50:17 +0300

    Update views.py
    топорный алгоритм, но работает

--- testTask/runningLine/views.py
+++ testTask/runningLine/views.py
@@ -18,40 +18,55 @@ def textGenerator(requeste, text):
 	infoBd = Request(userText = text, dateText = localtime(now()))#создаю объект модели Request
 	infoBd.save()#cохраняю в БД
 	# задаём параметры для отображения текста и записи видео
-	fps = 24#задаём количество кадров
+	ws_h = 400#высота видео в пикселях
+	ws_w = 400#ширина видео в пикселях
+	wh = ws_w/ws_h
+	fps = 24.0#задаём количество кадров
 	path_to_save = '/content/testTask_1'#путь сохранения видеофаййла
 	video_path = os.path.join(path_to_save, "video" + ".mp4")#соединяет путь с именем файла
 	font_face = cv2.FONT_HERSHEY_TRIPLEX#задание типа шрифта
 	font_scale = 8.0#размер шрифта
 	thickness = 8#толщина шрифта
-	fourcc = cv2.VideoWriter.fourcc('m', 'p', '4', 'v')#сохраняем файл с помощью видеокодека FourCC
+	fourcc = cv2.VideoWriter_fourcc(*'MP4V')#сохраняем файл с помощью видеокодека FourCC
 
 	# определяем размеры и цвет фонового изображения (background image)
 	(width, higth), _ = cv2.getTextSize(text, font_face, font_scale, thickness)#записываем размеры текста
-	bg_w, bg_h = 2*higth + width, 3 * higth#записываем размер фона
+	bg_w, bg_h = int((6*higth*wh)) + width, 3 * higth#записываем размер фона
 	bg_img = np.zeros((bg_h, bg_w, 3), np.uint8)#создание матрицы изображения
-	bg_img[:,:,1] = 200 # заполняем матрицу в зелёный цвет 
-	bg_img[:,:,0] = 13 # заполняем матрицу в синий цвет
+	bg_img[0:bg_h//2,:,1] = 200 # заполняем матрицу в зелёный цвет [y , x]
+	bg_img[:,0:bg_w//2,0] = 53 # заполняем матрицу в синий цвет
 	text_color = (255, 255, 255)#задаем цвет текста
 
 	# накладываем текст на фоновое изображение и приводим к нужному размеру
-	org = (int(higth / 2), int( 2*higth))#задаем координаты левого нижнего угла текста на картинке
+
+	koef = ws_w / bg_w#коэффициент перевода 
+	org = (int((3*higth*wh)), int(2 * higth))#int(higth / 2)#задаем координаты левого нижнего угла текста на картинке
 	img_w_text = cv2.putText(bg_img, text, org, font_face, font_scale, text_color, thickness)#рисуем строку текста на фоне 
-	target_img = cv2.resize(img_w_text, (int(100 * bg_w / bg_h), 100))#изменяем размер картинки под наши нужды
+	target_img = cv2.resize(img_w_text, (int(((bg_w/bg_h)*ws_h) + 2*ws_w) , ws_h)) #(int(((bg_w/bg_h)*ws_h)), ws_h))#изменяем размер картинки под наши нужды
 
 	# создаём VideoWriter и записываем кадры
-	num_frames = fps * 3#задаём количество кадров
-	step = (int(100 * bg_w / bg_h) - 100) / num_frames#задаём сдвиг
-	video = cv2.VideoWriter(video_path, fourcc, fps, (100, 100))#создаём объект VideoWriter для записи видео
-	
+	time = 3#длительность видео в секундах
+	num_frames = int(fps * time)#задаём количество кадров
+	#step = int((round(width*ws_h/bg_h) + ws_w)/num_frames)#(int(100 * bg_w / bg_h) - 100) / num_frames#задаём сдвиг
+	#step = int(((((bg_w/bg_h)*ws_h) - ws_w)/num_frames))
+	video = cv2.VideoWriter(video_path, fourcc, fps, (ws_w, ws_h))#создаём объект VideoWriter для записи видео
+	step = int((3*higth*wh + width)/num_frames)
 	#создаём цикл для покадровой записи
 	print('WRiting ', text)
+	print(width, higth, bg_w, bg_h, step,num_frames, int(((bg_w/bg_h)*ws_h)), len(target_img))
+	step = int((bg_w-3*higth)/num_frames)
 	for i in range(num_frames):
-		window_start = round(step * i)#задаём шаг
-		frame = target_img[:,window_start:window_start + 100]
+		bg_img = np.zeros((bg_h, bg_w, 3), np.uint8)
+		bg_img[0:,:,1] = 200 # заполняем матрицу в зелёный цвет [y , x]
+		bg_img[:,:,0] = 100 # заполняем матрицу в синий цвет
+		img_w_text = cv2.putText(bg_img, text, org, font_face, font_scale, text_color, thickness)#рисуем строку текста на фоне 
+		target_img = cv2.resize(img_w_text, (int(((bg_w/bg_h)*ws_h) + 2*ws_w) , ws_h)) #(int(((bg_w/bg_h)*ws_h)), ws_h))
+		frame = target_img[:,0:ws_w]
 		video.write(frame)#записываем кадр
+		org = (int((2.5*higth*wh)) - i*step, int(2 * higth))
+		print(org, i)
 	video.release()#освобождаю объект
-	print('End wRiting ', text)
+	
 	return HttpResponse(f"""
 			<h2>testTask</h2>
 			<p>Ваш текст: {text}</p>
